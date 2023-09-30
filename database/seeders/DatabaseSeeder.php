2cf077659901b967bdd4a5c668dbc4c78baf8934
rsanchez <rsanchez@isppro.net>
2023-09-30 13:00:06 -0600

    remove comments in seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,10 +15,10 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call([
-//            CategorySeeder::class,
-//            CourseSeeder::class,
+            CategorySeeder::class,
+            CourseSeeder::class,
             LessonSeeder::class,
-//            ProductSeeder::class
+            ProductSeeder::class
         ]);
     }
 }
